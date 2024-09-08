bb64aa65c12f41a674fccc9dd8bf5fe22f9ef3ec
Joshua Kitchen <57072030+kitchej@users.noreply.github.com>
2024-09-08 12:41:43 -0700

    Accident deleted self.client_id from Message class

--- src/TCPLib/message.py
+++ src/TCPLib/message.py
@@ -8,6 +8,7 @@ class Message:
     """
     Container class for holding the size and data of a message
     """
-    def __init__(self, size, data):
+    def __init__(self, size, data, client_id=None):
         self.size = size
         self.data = data
+        self.client_id = client_id
